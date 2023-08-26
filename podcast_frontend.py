e6130803acca499f34f132715626409fbcd5beb6
AyowoleT <techytape@gmail.com>
2023-08-27 00:40:53 +0100

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -42,14 +42,11 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            #added code
-            st.write("This is the length ",  podcast_info['podcast_guest'])
-            #addition end
             st.write(podcast_info['podcast_guest'])
 
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
+            st.write(podcast_info["podcast_guest"])
 
         # Display the five key moments
         st.subheader("Key Moments")
